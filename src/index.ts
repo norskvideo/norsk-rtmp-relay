3ac5ffb5e57c325e06b7c121eaf1c795c917fe9a
robashton <robashton@codeofrob.com>
2025-08-05 10:04:57 +0100

    clear up and re-create rtmp egests on demand

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Norsk, OnStreamResult } from '@norskvideo/norsk-sdk';
+import { Norsk, OnStreamResult, RtmpOutputNode } from '@norskvideo/norsk-sdk';
 import Config from './config';
 import { debuglog, errorlog, infolog } from './logging';
 
@@ -7,6 +7,7 @@ const stayAlive = setTimeout(() => { }, 60000);
 
 async function main() {
   debuglog("Connecting to Norsk");
+  const active = new Map<string, { app: string, publishName: string, relays: RtmpOutputNode[] }>();
 
   const norsk = await Norsk.connect({
     url: `${Config.norsk.host()}:${Config.norsk.api_port()}`,
@@ -35,12 +36,17 @@ async function main() {
         return { accept: false, reason: "unknown app" }
       }
     },
-    onStream: (_connectionId, app, _url, streamId, publishName): OnStreamResult => {
+    onConnectionStatusChange: (connectionId, status) => {
+      if (status !== 'disconnected') return;
+      void stopStreamsForSource(connectionId);
+    },
+    onStream: (connectionId, app, _url, streamId, publishName): OnStreamResult => {
       const matching = streams.filter((s) => {
         return s.from.app == app && s.from.stream == publishName
       });
       if (matching.length > 0) {
         infolog("Accepting stream for app", { app, streamId, publishName })
+        void startStreamsForSource(connectionId, app, publishName);
         return {
           accept: true,
           videoStreamKey: {
@@ -64,25 +70,49 @@ async function main() {
     }
   })
 
-  streams.map(async (stream, i) => {
-    infolog("Setting up stream egest", { from: stream.from, to: stream.to })
-    const relay = await norsk.output.rtmp({
-      id: `egest-${stream.from.stream}-${stream.to.stream}-${i}`,
-      url: `rtmp://${stream.to.host}:${stream.to.port}/${stream.to.app}/${stream.to.stream}`,
-    })
-    relay.subscribe([
-      {
-        source: ingest,
-        sourceSelector: (streams) => {
-          return streams
-            .filter((s) =>
-              s.streamKey.sourceName == `${stream.from.app}-${stream.from.stream}`)
-            .map((s => s.streamKey))
+  async function startStreamsForSource(connectionId: string, app: string, publishName: string) {
+    const v = active.get(connectionId);
+    if (v) {
+      errorlog("Stream already connected, this is a bug", { app, publishName });
+      return;
+    }
+    const newV = { app, publishName, relays: new Array<RtmpOutputNode>() };
+    active.set(connectionId, newV);
+    streams.map(async (stream, i) => {
+      if (stream.from.app == app && stream.from.stream == publishName) {
+        infolog("Setting up stream egest", { from: stream.from, to: stream.to })
+        const relay = await norsk.output.rtmp({
+          id: `egest-${stream.from.stream}-${stream.to.stream}-${i}`,
+          url: `rtmp://${stream.to.host}:${stream.to.port}/${stream.to.app}/${stream.to.stream}`,
+        })
+        newV.relays.push(relay);
+        relay.subscribe([
+          {
+            source: ingest,
+            sourceSelector: (streams) => {
+              return streams
+                .filter((s) =>
+                  s.streamKey.sourceName == `${stream.from.app}-${stream.from.stream}`)
+                .map((s => s.streamKey))
 
-        }
+            }
+          }
+        ])
       }
-    ])
-  })
+    })
+  }
+
+  async function stopStreamsForSource(connectionId: string) {
+    const v = active.get(connectionId);
+    if (!v) {
+      errorlog("Unrecognised stream disconnected");
+      return;
+    }
+    active.delete(connectionId);
+    Promise.all(v.relays.map(async (r) => r.close()));
+  }
 }
 
 void main();
+
+
